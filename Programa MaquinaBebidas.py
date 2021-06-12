b4d0f814433a6407c91f7961c02296dc0d546513
Hernan <hl586@hotmail.com>
2021-06-12 14:19:10 -0600

    Modificaciones para el programa 'Máquina de bebidas'

--- Programa MaquinaBebidas.py
+++ Programa MaquinaBebidas.py
@@ -1,19 +1,24 @@
 def maquina_bebidas(nombre, cantidad):
-    stock = 0
+    # stock = 0
     monto = 0
+    total = 10
     print(f"Ha seleccionado {nombre}")
     if cantidad <= 0:
         print("Productos agotados")
     else:
         print("Total a pagar: ", 10)
-
         while monto < 10:
             monedas = int(input("Deposite moneda (acepta $1 $2 $5 $10): "))
-            monto = monto + monedas
-            print("Total depositado: $", monto)
+            if monedas != 1 and monedas != 2 and monedas != 5 and monedas != 10:
+                print("\u001b[31;1mLa moneda es incorrecta, intentelo de nuevo\u001b[0m")
+            else:
+                monto = monto + monedas
+                total -= monedas
+                print("Total depositado: $", monto, "\nFalta por pagar: ", total)
+
 
             if monto == 10:
-                print("Recoja su producto")
+                print("\u001b[32;1mHa completado el pago exitosamente\nRecoja su producto\u001b[0m")
         stock = cantidad - 1
         print("Productos en stock", stock)
 
@@ -32,28 +37,36 @@ bebida = 0
 
 
 while bebida != 5:
-    print("\nMenu refrescos\n"
-          "1.Coca cola\n"
-          "2.Fanta\n"
-          "3.Manzanita\n"
-          "4.Sprite\n"
-          "5.Coca cola light")
-    bebida = int(input("Ingrese una opcion: "))
+    try:
+        print("\nMenu refrescos\n"
+              "1.Coca cola\n"
+              "2.Fanta\n"
+              "3.Manzanita\n"
+              "4.Sprite\n"
+              "5.Coca cola light\n"
+              "6.Salir")
+        bebida = int(input("Ingrese una opcion: "))
 
-    if bebida == 1:
-        cocacola = maquina_bebidas("Coca Cola", cocacola)
+        if bebida == 1:
+            cocacola = maquina_bebidas("Coca Cola", cocacola)
 
-    elif bebida == 2:
-        fanta = maquina_bebidas("Fanta", fanta)
+        elif bebida == 2:
+            fanta = maquina_bebidas("Fanta", fanta)
 
-    elif bebida == 3:
-        manzanita = maquina_bebidas("Manzanita", manzanita)
+        elif bebida == 3:
+            manzanita = maquina_bebidas("Manzanita", manzanita)
 
-    elif bebida == 4:
-        sprite = maquina_bebidas("Sprite", sprite)
+        elif bebida == 4:
+            sprite = maquina_bebidas("Sprite", sprite)
 
-    elif bebida == 5:
-        cocacolalight = maquina_bebidas("Coca Cola light", cocacolalight)
+        elif bebida == 5:
+            cocacolalight = maquina_bebidas("Coca Cola light", cocacolalight)
 
-    else:
-        print("Numero incorrecto, intente de nuevo\n")
+        elif bebida == 6:
+            print("Saliendo del programa....")
+            break
+
+        else:
+            print("\u001b[31;1mEl dato infresado es incorrecto, intente de nuevo\n\u001b[0m")
+    except:
+        print("\u001b[31;1mEl dato infresado es incorrecto, intente de nuevo\n\u001b[0m")
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Maquina_bebidas
+#Información del programa
+Nombre del programa: Máquina de bebidas
+Autor: Hernán Araya
+
+#Descripción:
+Este es programa consiste en una máquina expendedora de bebidas, en donde la persona
+selecciona una bebida y debe pagarla con las monedas que la máquina acepta.
+
+#Instrucciones del programa
+En el menú principal encontrará una lista con las bebidas:
+1.Coca cola
+2.Fanta
+3.Manzanita
+4.Sprite
+5.Coca cola light
+
+Deberá seleccionar una opción, seguidamente el programa le mostrará un mensaje para que realice 
+el pago de dicha bebida con las monedas que la máquina acepta hasta que la suma de dichas monedas
+sea 10, que es el total que cuesta cada bebida.
+El mismo programa mostrará cuantas bebidas quedan en stock 
